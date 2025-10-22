30ed0004b2dea063b627abc8118bb091f9fdb7bd
sameedahri <sameedahri48@gmail.com>
2025-10-22 15:33:11 +0500

    Refactor hero section styles for improved layout and responsiveness. Updated input group height and button dimensions for better usability. Enhanced text styling and added backdrop blur effects for visual appeal.

--- src/features/home/component/section/hero-section.tsx
+++ src/features/home/component/section/hero-section.tsx
@@ -14,7 +14,7 @@ import { Button } from '@/shared/components/ui/button'
 
 function HeroSection() {
     return (
-        <div className="relative min-h-[850px] overflow-hidden grid">
+        <div className="relative py-10 min-h-[800px] overflow-hidden grid">
             <Image
                 src={home.heroImage}
                 alt="Hero"
@@ -28,21 +28,23 @@ function HeroSection() {
                 // className="absolute inset-0 -z-[5] pointer-events-none bg-[linear-gradient(to_bottom,transparent_0%,rgba(255,255,255,0.05)_60%,rgba(255,255,255,0.12)_100%)]"
                 className="absolute inset-0 bg-linear-to-b from-transparent via-transparent to-white/98 -z-[5] pointer-events-none "
             ></div>
-            <div className="container  h-full relative w-full flex items-center">
+            <div className="container h-full relative w-full flex items-center">
                 <div className={cn(
-                    "backdrop-blur-md bg-white/75 rounded-md px-10 py-10 flex flex-col gap-5 w-[80%] mx-auto",
-                    "shadow-[0px_0px_10px_0px_rgba(255,255,255, 0.5)]"
+                    "rounded-md px-6 py-6 flex flex-col gap-5 mx-auto w-full md:py-10 md:px-10 xl:w-[80%]",
+                    
+                    // "shadow-[0px_0px_10px_0px_rgba(255,255,255, 0.5)]",
+                    "backdrop-blur-sm border bg-linear-to-b to-transparent from-white/10 border-white/10 border-t-white/5 border-l-white/5 "
                 )}>
-                    <h1 className="text-3xl text-center font-bold  text-primary md:text-4xl">Find. Connect. Succeed.</h1>
+                    <h1 className="text-3xl text-center font-bold font-heading  text-white text-shadow-lg md:text-5xl lg:text-5xl xl:text-6xl">Find. Connect. <strong className=''>Succeed.</strong></h1>
                     <Tabs defaultValue="agents" className='items-center gap-5'>
-                        <TabsList className='bg-background-light/60'>
+                        <TabsList className='bg-background-light/60 backdrop-blur-2xl'>
                             <TabsTrigger value="agents">Agents</TabsTrigger>
                             <TabsTrigger value="agencies">Agencies</TabsTrigger>
                             <TabsTrigger value="services">Services</TabsTrigger>
                             <TabsTrigger value="events">Events</TabsTrigger>
                         </TabsList>
                         <TabsContent value="agents" className='w-full'>
-                            <div className='flex gap-3'>
+                            <div className='flex flex-wrap gap-3 bg-background-light/60 backdrop-blur-2xl rounded-md p-5 lg:flex-nowrap'>
                                 <InputGroup
                                     className='bg-white'
                                 >
@@ -50,7 +52,7 @@ function HeroSection() {
                                         <Search />
                                     </InputGroupAddon>
                                     <InputGroupInput
-                                        placeholder='Search by name' 
+                                        placeholder='Search by specialty or name' 
                                         // className=' w-full'
                                     />
                                 </InputGroup>
@@ -71,7 +73,7 @@ function HeroSection() {
                                     // className=' w-full'
                                     />
                                 </InputGroup>
-                                <Button size={"sm"} className='h-9'>
+                                <Button size={"sm"} className='h-11 w-32'>
                                     <Search />
                                     Search
                                 </Button>
